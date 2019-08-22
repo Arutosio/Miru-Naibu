c0586fb4d2a125f00080e1928a3f5998683a06c2
Arutosio <Arutosio@outlook.com>
2019-08-22 15:30:42 +0200

    Fix User Singleton now it is thread-safety

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,16 +1,22 @@
 namespace Miru_Naibu.Entities
 {
-    public class User
+    public sealed class User
     {
         private static User instance = null;
+        private static readonly object padlock = new object();
         private User() {
             Username = "fileUser";
+            Password = "fileMD5password";
         }
-        public static User GetUserInstance() {
-            if(instance != null) {
-                return new User();
-            } 
-            return instance;
+        public static User GetUserInstance { 
+            get {
+                lock (padlock) {
+                    if(instance != null) {
+                        return new User();
+                    } 
+                    return instance;
+                }
+            }
         }
         public string Username { get; private set; }
         public string Password { get; private set; }
