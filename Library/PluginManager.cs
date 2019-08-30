d18d51a306db90a8253d15689ef11f2f75b79709
Arutosio <Arutosio@outlook.com>
2019-08-30 16:29:24 +0200

    method created

--- Library/PluginManager.cs
+++ Library/PluginManager.cs
@@ -8,6 +8,8 @@ using PluginBase;
 namespace Miru_Naibu.Library
 {
     public static class PluginManager {
+        public static string[] pluginPaths = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Plugins"), "*.dll");
+        public static IEnumerable<ICommand> commands;
         public static void Start(string[] args)
         {
             try {
@@ -16,11 +18,7 @@ namespace Miru_Naibu.Library
                     Console.ReadLine();
                 }
                 //START LOAD commands from plugins.
-                string[] pluginPaths = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "Plugins"), "*.dll");
-                IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath => {
-                    Assembly pluginAssembly = LoadPlugin(pluginPath);
-                    return CreateCommands(pluginAssembly);
-                }).ToList();
+                ReloadPlugins();
                 //END LOAD commands
                 while(true) {
                     args = Program.CmdSplit(Console.ReadLine());
@@ -46,37 +44,43 @@ namespace Miru_Naibu.Library
                     }
                 }
             } catch (Exception ex) { Console.WriteLine(ex); }
-            static Assembly LoadPlugin(string relativePath) {
-                // Navigate up to the solution root 
-                string root = Path.GetFullPath(Path.Combine(
+        }
+        public static void ReloadPlugins() {
+            commands = pluginPaths.SelectMany(pluginPath => {
+                Assembly pluginAssembly = LoadPlugin(pluginPath);
+                return CreateCommands(pluginAssembly);
+            }).ToList();
+        }
+        public static Assembly LoadPlugin(string relativePath) {
+            // Navigate up to the solution root 
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
                     Path.GetDirectoryName(
                         Path.GetDirectoryName(
                             Path.GetDirectoryName(
-                                Path.GetDirectoryName(
-                                    Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
-                string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
-                Console.WriteLine($"Loading commands from: {pluginLocation}");
-                PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
-                return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
-            }
-            static IEnumerable<ICommand> CreateCommands(Assembly assembly) {
-                int count = 0;
-                foreach (Type type in assembly.GetTypes()) {
-                    if (typeof(ICommand).IsAssignableFrom(type)) {
-                        ICommand result = Activator.CreateInstance(type) as ICommand;
-                        if (result != null) {
-                            count++;
-                            yield return result;
-                        }
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading commands from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+        public static IEnumerable<ICommand> CreateCommands(Assembly assembly) {
+            int count = 0;
+            foreach (Type type in assembly.GetTypes()) {
+                if (typeof(ICommand).IsAssignableFrom(type)) {
+                    ICommand result = Activator.CreateInstance(type) as ICommand;
+                    if (result != null) {
+                        count++;
+                        yield return result;
                     }
                 }
-                if (count == 0) {
-                    string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
-                    throw new ApplicationException(
-                        $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
-                        $"Available types: {availableTypes}"
-                    );
-                }
+            }
+            if (count == 0) {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}"
+                );
             }
         }
         public static bool CheckDirPlugins() {
